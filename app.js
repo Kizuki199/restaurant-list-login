58a681664f32e698c6034ca5d340635f83f5bf67
Henry Chung <chungjenhao81@gmail.com>
2021-01-25 08:47:05 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const hbsHelpers = require('./helpers/handlebars')
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const flash = require('connect-flash')
 
 const routes = require("./routes")
 
@@ -33,10 +34,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
